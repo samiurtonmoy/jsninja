544809ee06855f2b79d1f917eb942defaf00ec67
samiurtonmoy <samiur0711@gmail.com>
2016-06-07 17:40:55 +0600

    initial commit with added index css js and readme file

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// welcome the user
+// alert("Welcome to Quiz Ninja!");
+
+// var question = "What is Superman's real name?";
+//
+// var answer = prompt(question);
+//
+// alert("You answered " + answer);
+
+// var quiz =
+// [
+//   ["What is Superman's real name?","Clarke Kent"],
+//   ["What is Wonderwoman's real name?","Dianna Prince"],
+//   ["What is Batman's real name?","Bruce Wayne"]
+// ];
+//
+// var score = 0; // initialize score
+//
+//
+// for(var i=0,max=quiz.length;i<max;i++){
+//   // get answer from user
+//   var answer = prompt(quiz[i][0]); // quiz[i][0] is the ith question
+//   // check if answer is correct
+//   if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
+//     alert("Correct!");
+//     // increase score by 1
+//     score++;
+//   } else {
+//     alert("Wrong!");
+//   }
+// }
+//
+// alert("Game Over, you scored " + score + " points");
