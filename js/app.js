84480956feb099eb7e6ffae82d3eb0186bfa5e86
samiurtonmoy <samiur0711@gmail.com>
2016-06-07 17:51:54 +0600

    changed some code in app.js file

--- js/app.js
+++ js/app.js
@@ -1,33 +1,40 @@
-// welcome the user
-// alert("Welcome to Quiz Ninja!");
+// Quiz Game
+var quiz = [
+   ["What is Superman's real name?","Clarke Kent"],
+   ["What is Wonderwoman's real name?","Dianna Prince"],
+   ["What is Batman's real name?","Bruce Wayne"]
+];
 
-// var question = "What is Superman's real name?";
-//
-// var answer = prompt(question);
-//
-// alert("You answered " + answer);
+var score = 0; // initialize score
 
-// var quiz =
-// [
-//   ["What is Superman's real name?","Clarke Kent"],
-//   ["What is Wonderwoman's real name?","Dianna Prince"],
-//   ["What is Batman's real name?","Bruce Wayne"]
-// ];
-//
-// var score = 0; // initialize score
-//
-//
-// for(var i=0,max=quiz.length;i<max;i++){
-//   // get answer from user
-//   var answer = prompt(quiz[i][0]); // quiz[i][0] is the ith question
-//   // check if answer is correct
-//   if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
-//     alert("Correct!");
-//     // increase score by 1
-//     score++;
-//   } else {
-//     alert("Wrong!");
-//   }
-// }
-//
-// alert("Game Over, you scored " + score + " points");
+play(quiz);
+
+function play(quiz) {
+// main game loop
+for(var i=0, question, answer, max=quiz.length; i<max; i++) {
+question = quiz[i][0];
+answer = ask(question);
+check(answer);
+}
+// end of main game loop
+gameOver();
+}
+
+function ask(question) {
+    return prompt(question); // quiz[i][0] is the ith questions
+}
+
+function check(answer) {
+if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
+alert("Correct!");
+// increase score by 1
+score++;
+} else {
+alert("Wrong!");
+}
+}
+
+function gameOver(){
+// inform the player that the game has finished and tell them how many points they have scored
+alert("Game Over, you scored " + score + " points");
+}
